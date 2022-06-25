f8e58927e10e3f426913b54267b7bd18326f6369
f-deniyi <holarfemilekan049@gmail.com>
2022-06-25 12:48:46 +0100

    updates

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,139 +1,155 @@
 <template>
   <AppLayout>
-    <div className='dashboard-container'>
-    <div className="mb-4 ">
-      <div className=" dashboard-cards-container ">
-        <div className="card  border-none ">
-          <img src="../../assets/card.png" className="card-grad-img" />
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="">
-              <h6 className="mb-0">Products</h6>
-              <h5>3</h5>
-            </div>
-            <div>
-              <img src="../../assets/1.png" width="80" />
-            </div>
-          </div>
-        </div>
-        <div className="card  border-none ">
-          <img src="../../assets/card.png" className="card-grad-img" />
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="">
-              <h6 className="mb-0">Sales</h6>
-              <h5>₦16,345.00</h5>
-            </div>
-            <div>
-              <img src="../../assets/2.png" width="80" />
+    <div className="dashboard-container">
+      <div className="mb-4 ">
+        <div className=" dashboard-cards-container ">
+          <div className="card  border-none ">
+            <img src="../../assets/card.png" className="card-grad-img" />
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="">
+                <h6 className="mb-0">Products</h6>
+                <h5>3</h5>
+              </div>
+              <div>
+                <img src="../../assets/1.png" width="80" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card  border-none ">
-          <img src="../../assets/card.png" className="card-grad-img" />
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="">
-              <h6 className="mb-0">Posts</h6>
-              <h5>177</h5>
-            </div>
-            <div>
-              <img src="../../assets/3.png" width="80" />
+          <div className="card  border-none ">
+            <img src="../../assets/card.png" className="card-grad-img" />
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="">
+                <h6 className="mb-0">Sales</h6>
+                <h5>₦16,345.00</h5>
+              </div>
+              <div>
+                <img src="../../assets/2.png" width="80" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card  border-none ">
-          <img src="../../assets/card.png" className="card-grad-img" />
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="">
-              <h6 className="mb-0">Users</h6>
-              <h5>107</h5>
+          <div className="card  border-none ">
+            <img src="../../assets/card.png" className="card-grad-img" />
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="">
+                <h6 className="mb-0">Posts</h6>
+                <h5>177</h5>
+              </div>
+              <div>
+                <img src="../../assets/3.png" width="80" />
+              </div>
             </div>
-            <div>
-              <img src="../../assets/4.png" width="80" />
+          </div>
+          <div className="card  border-none ">
+            <img src="../../assets/card.png" className="card-grad-img" />
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="">
+                <h6 className="mb-0">Users</h6>
+                <h5>107</h5>
+              </div>
+              <div>
+                <img src="../../assets/4.png" width="80" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="row">
-      <div className="col-sm-12 col-md-7">
-        <div
-          className="d-sm-block d-md-flex justify-content-between align-items-center post-header  py-1 mb-1"
-        >
-          <div class="d-flex mb-2">
-            <img src="../../assets/chat.png" />
+      <div className="row">
+        <div className="col-sm-12 col-md-12 col-lg-7">
+          <div
+            className="d-sm-block d-md-flex justify-content-between align-items-center post-header  py-1 mb-1"
+          >
+            <div class="d-flex mb-2">
+              <img src="../../assets/chat.png" />
 
-            <p>Recent General Feeds</p>
-          </div>
-          <div className="feed-search-container">
-            <div className="search-icon">
-              <img src="../../assets/search.png" />
+              <p>Recent General Feeds</p>
             </div>
-            <input type="text" className="search-field" placeholder="Search" />
-          </div>
-        </div>
-        <div className="feeds">
-          <div className="mb-1 border-bottom pt-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex align-items-center">
-                <img className="user-dp" src="../../assets/F1.png" />
-                <h6 class="mb-0 feed-poster">Moses Olaniyi</h6>
+            <div className="feed-search-container">
+              <div className="search-icon">
+                <img src="../../assets/search.png" />
               </div>
-              <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
+              <input
+                type="text"
+                className="search-field"
+                placeholder="Search"
+              />
             </div>
-            <p className="feed my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              et facilisis mi id ultrices. Tortor eleifend diam ut aenean eget
-              diam. Nullam et facilisi non venenatis. Molestie lectus facilisi
-              sed laoreet faucibus.
-            </p>
           </div>
-          <div className="mb-1 border-bottom pt-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex align-items-center">
-                <img className="user-p" width='30' src="../../assets/F2.png"/>
-                <h6 class="mb-0 feed-poster">Julliet Chinwere</h6>
+          <div className="feeds">
+            <div className="mb-1 border-bottom pt-3">
+              <div
+                className="d-flex justify-content-between align-items-center"
+              >
+                <div className="d-flex align-items-center">
+                  <img className="user-dp" src="../../assets/F1.png" />
+                  <h6 class="mb-0 feed-poster">Moses Olaniyi</h6>
+                </div>
+                <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
               </div>
-              <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
+              <p className="feed my-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Faucibus et facilisis mi id ultrices. Tortor eleifend diam ut
+                aenean eget diam. Nullam et facilisi non venenatis. Molestie
+                lectus facilisi sed laoreet faucibus.
+              </p>
             </div>
-            <p className="feed my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              et facilisis mi id ultrices. Tortor eleifend diam ut aenean eget
-              diam. Nullam et facilisi non venenatis. Molestie lectus facilisi
-              sed laoreet faucibus.
-            </p>
-          </div>
-          <div className="mb-1 border-bottom pt-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex align-items-center">
-                <img className="user-dp" src="../../assets/user.png" />
-                <h6 class="mb-0 feed-poster">Super Admin</h6>
+            <div className="mb-1 border-bottom pt-3">
+              <div
+                className="d-flex justify-content-between align-items-center"
+              >
+                <div className="d-flex align-items-center">
+                  <img
+                    className="user-p"
+                    width="30"
+                    src="../../assets/F2.png"
+                  />
+                  <h6 class="mb-0 feed-poster">Julliet Chinwere</h6>
+                </div>
+                <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
               </div>
-              <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
+              <p className="feed my-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Faucibus et facilisis mi id ultrices. Tortor eleifend diam ut
+                aenean eget diam. Nullam et facilisi non venenatis. Molestie
+                lectus facilisi sed laoreet faucibus.
+              </p>
             </div>
-            <p className="feed my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              et facilisis mi id ultrices. Tortor eleifend diam ut aenean eget
-              diam. Nullam et facilisi non venenatis. Molestie lectus facilisi
-              sed laoreet faucibus.
-            </p>
-          </div>
-          <div className="mb-1  pt-3">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex align-items-center">
-                <img className="user-dp" src="../../assets/F1.png" />
-                <h6 class="mb-0 feed-poster">Moses Olaniyi</h6>
+            <div className="mb-1 border-bottom pt-3">
+              <div
+                className="d-flex justify-content-between align-items-center"
+              >
+                <div className="d-flex align-items-center">
+                  <img className="user-dp" src="../../assets/user.png" />
+                  <h6 class="mb-0 feed-poster">Super Admin</h6>
+                </div>
+                <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
               </div>
-              <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
+              <p className="feed my-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Faucibus et facilisis mi id ultrices. Tortor eleifend diam ut
+                aenean eget diam. Nullam et facilisi non venenatis. Molestie
+                lectus facilisi sed laoreet faucibus.
+              </p>
             </div>
-            <p className="feed my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              et facilisis mi id ultrices. Tortor eleifend diam ut aenean eget
-              diam. Nullam et facilisi non venenatis. Molestie lectus facilisi
-              sed laoreet faucibus.
-            </p>
+            <div className="mb-1  pt-3">
+              <div
+                className="d-flex justify-content-between align-items-center"
+              >
+                <div className="d-flex align-items-center">
+                  <img className="user-dp" src="../../assets/F1.png" />
+                  <h6 class="mb-0 feed-poster">Moses Olaniyi</h6>
+                </div>
+                <p className="feed-date">Tue 24 May, 2022 04:01:22</p>
+              </div>
+              <p className="feed my-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Faucibus et facilisis mi id ultrices. Tortor eleifend diam ut
+                aenean eget diam. Nullam et facilisi non venenatis. Molestie
+                lectus facilisi sed laoreet faucibus.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </AppLayout>
 </template>
@@ -316,7 +332,7 @@ p {
   font-size: 16.2908px;
   /* margin-right: 2px; */
   color: #000000;
-  margin-left:6px;
+  margin-left: 6px;
 }
 .feed {
   font-weight: 500;
@@ -324,38 +340,43 @@ p {
 
   color: #403c3c;
 }
-@media(max-width:768px){
-  .main{
+@media (max-width: 768px) {
+  .main {
     margin: 0 0 0 0;
-  /* background: black; */
-  /* height: 100vh; */
-  padding: 0 ;
+    /* background: black; */
+    /* height: 100vh; */
+    padding: 0;
   }
   .dashboard-cards-container {
-  display: grid;
-  grid-template-columns: minmax(0, 1fr) ;
-  grid-gap: 12px;
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+    grid-gap: 12px;
+  }
+  .dashboard-container {
+    margin: auto;
+    width: 90%;
+  }
+  .feed-date {
+    font-weight: 400;
+    font-size: 12px;
+    color: #6b6a6a;
+  }
+  .feed-poster {
+    font-weight: 400;
+    font-size: 13.908px;
+    margin-right: 2px;
+    color: #000000;
+  }
+  .feed-search-container {
+    /* width:%; */
+    margin-bottom: 5px;
+  }
 }
-.dashboard-container{
-  margin:auto;
-  width:90%;
+@media (max-width: 1030px) {
+  .dashboard-cards-container {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+    grid-gap: 12px;
+  }
 }
-.feed-date {
-  font-weight: 400;
-  font-size: 12px;
-  color: #6b6a6a;
-}
-.feed-poster {
-  font-weight: 400;
-  font-size: 13.908px;
-  margin-right: 2px;
-  color: #000000;
-}
-.feed-search-container {
-  /* width:%; */
-  margin-bottom:5px;
-}
-
-}
-
 </style>
